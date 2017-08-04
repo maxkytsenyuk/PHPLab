c2fb8857c4d2d85bdbefb76f635b2a683053078a
maxkytsenyuk <max.kytsenyuk@gmail.com>
2017-08-04 21:48:21 +0300

    Optimized Task #4 algorithm

--- Lab2/Lab2_MainTask_4.php
+++ Lab2/Lab2_MainTask_4.php
@@ -1,21 +1,10 @@
 <?php
 
 $a = [342, 55, 33, 123, 66, 63, 9];
-$counter = 0;
 
-for ($i = 0; $i < count($a); $i++) {
-    (strpos($a[$i], '3') === false) ? $counter : $counter += countProcess($a[$i]);
-
-
+$arrayCounter = count($a);
+$count = 0;
+for ($i = 0; $i < $arrayCounter-1; $i++) {
+    $count = $count + substr_count($a[$i], '3');
 }
-echo $counter;
-
-function countProcess($number)
-{
-    $counter = 0;
-    for ($i = 0; $i < (strlen($number)); $i++) {
-        (substr($number, $i, 1) == '3') ? $counter++ : $counter;
-    }
-
-    return $counter;
-}
+echo $count;
