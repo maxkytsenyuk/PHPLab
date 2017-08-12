bf6d192384a7649c32f42a91e04863965b60c9f6
maxkytsenyuk <max.kytsenyuk@gmail.com>
2017-08-12 08:17:58 +0300

    Task #4 fixes

--- Lab4/Lab4_MainTask_6_ImprovedAlgorithm.php
+++ Lab4/Lab4_MainTask_6_ImprovedAlgorithm.php
@@ -3,21 +3,23 @@
 $string = 'raersrrrersassswwaaadfdfeeefgtthtgffdd';
 $max = 0;
 $maxChar = '';
-$length = strlen($string);
+$length = strlen($string) - 1;
 
 for ($i = 0; $i < $length; $i++) {
 
     for ($j = $length; $i < $j; $j--) {
         $currentDistance = $j - $i;
 
-        if ($string[$i] == $string[$j] && ($currentDistance > $max)) {
+        if ($max > $currentDistance) {
+            break;
+        } elseif ($string[$i] == $string[$j] && ($currentDistance >= $max)) {
             $max = $currentDistance;
             $maxChar = $string[$i];
             break;
         }
     }
 
-    if ($max > $length - ($i + $j)) {
+    if ($max > $length - ($i + 1)) {
         break;
     }
 }
